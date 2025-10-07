1777d3da727302fa438af812437285aa9e43e77c
Akash <itsmeakash248@gmail.com>
2025-10-07 10:31:16 +0530

    start of the project!

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,10 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include "LGFX.hpp"
-
-LGFX lcd;
 
 extern "C" void app_main(void) {
-    // Initialize the display
-    lcd.init();
-    lcd.setRotation(1);
-    lcd.setBrightness(128);
-    lcd.clear();
+    printf("Welcome !\n");
     
-    lcd.setTextColor(lcd.color565(255, 255, 255));
-    lcd.setTextSize(2);
-    lcd.setTextDatum(middle_centre);
-    lcd.drawString("Hello LovyanGFX!", lcd.width() / 2, lcd.height() / 2);
-    vTaskDelay(pdMS_TO_TICKS(10));
-
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
